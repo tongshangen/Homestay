19b045e308ec0a9cf3f8e06d4636f67304076c62
CTS <1393676091@qq.com>
2017-12-07 17:39:51 +0800

    qw

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,38 +1,15 @@
 <template>
   <div id="app">
-    <!-- <MainComponent></MainComponent> -->
-<<<<<<< HEAD
-
-    <!--  下面div为测试能否引入element.ui 记得删除 
-    <div>
-      <el-button round>圆形按钮</el-button>
-      <el-button type="primary" round>主要按钮</el-button>
-      <el-button type="success" round>成功按钮</el-button>
-      <el-button type="info" round>信息按钮</el-button>
-      <el-button type="warning" round>警告按钮</el-button>
-      <el-button type="danger" round>危险按钮</el-button>
-    </div>-->
-=======
->>>>>>> 5b2e4ae7c6c3dbcb58a2bd5eebd03c84366f839c
+  
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-=======
-  // import MainComponent from "./components/main/MainComponent.vue"
->>>>>>> 5b2e4ae7c6c3dbcb58a2bd5eebd03c84366f839c
   export default {
     name: 'app',
     data () {
       return {}
     }
-<<<<<<< HEAD
-=======
-    // components: {
-    //     MainComponent
-    //   }
->>>>>>> 5b2e4ae7c6c3dbcb58a2bd5eebd03c84366f839c
   }
 </script>
