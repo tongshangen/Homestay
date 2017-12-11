30ed9d73cef3716df630c1fae16e3e49fe9fb37b
tsg <350298719@qq.com>
2017-12-11 17:03:58 +0800

    sessionStorage

--- app/src/main.js
+++ app/src/main.js
@@ -3,14 +3,22 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router/index'
+import Vuex from 'vuex'
 import jquery from 'jquery'
 
 
-
 Vue.use(ElementUI)
+Vue.use(Vuex)
+
+var store = new Vuex.Store({
+ state: {
+    user_tel: ''
+ }
+})
 
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
