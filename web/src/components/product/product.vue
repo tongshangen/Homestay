7bf6e4b50b4566bb4274317761a980bd9b5fe89e
JoeImJoe <1334942535@qq.com>
2017-12-11 15:10:43 +0800

    修改

---
+++ web/src/components/product/product.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h1>123</h1>
+		<div>
+	        <datagrid api="http://localhost:999/PHP/student.php" :filterCols="filterColumns"></datagrid>
+	    </div>
+	</div>
+</template>
+
+<script>
+	import datagrid from '../datagrid/datagrid.vue'
+
+    export default {
+        data: function(){
+            return {
+                filterColumns: []
+            }
+        },
+        components: {
+            datagrid
+        }
+    }
+</script>
